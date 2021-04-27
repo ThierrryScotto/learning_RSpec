72e9aba598df1dbc5527bcd508da4acbbf7025d2
ThierrryScotto <thierry_192@outlook.com>
2021-04-26 22:40:49 -0300

    using hook before

--- readme.md
+++ readme.md
@@ -15,3 +15,15 @@ rspec <file_path> --fail-fast
 equal? and eql? | this is function not are the same thing:
 equal: compare to see if the object is ta same object
 eql: see the information inside in the variable
+
+# hooks
+
+- executa a cada bloco
+before(:example) do
+  @car = Car.new
+end
+
+- executa uma vez
+before(:context) do
+  @car = Car.new
+end
---
+++ spec/car_spec_hooks.rb
@@ -0,0 +1,49 @@
+require 'car'
+
+describe 'Car' do
+
+  before(:example) do
+    @car = Car.new
+  end
+
+  it "Allows reading and writing for :make" do
+    @car.make = 'Test'
+    expect(@car.make).to eq('Test')
+  end
+
+  it "allow writing and reading for :year" do
+    @car.year = 9999
+    expect(@car.year).to eq(9999)
+  end
+
+  it "allow writing and reading for :color" do
+    @car.color = 'foo'
+    expect(@car.color).to eq('foo')
+  end
+
+  it "allow reading for wheels" do
+    expect(@car.wheels).to eq(4)
+  end
+
+  describe '.colors' do
+    it "Returns an array of color names" do
+      c = ['blue', 'black', 'red', 'green']
+      expect(Car.colors).to match_array(c)
+    end
+  end
+
+  describe '#full_name' do
+    it "Returns a string in the expected format" do
+      @honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
+      expect(@honda.full_name).to eq('2004 Honda (blue)')
+    end
+
+    context 'when initialized with no arguments' do
+      it 'returns a string using default values' do
+        car = Car.new
+        expect(car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
+  end
+
+end
